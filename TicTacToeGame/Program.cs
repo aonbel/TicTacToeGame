4e572e199287ce7ba31379f3446ffe43d0b74c83
Anioni-12 <anbel13219@gmail.com>
2021-09-20 15:44:21 +0300

    Game mechanics were implemented(beta)

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -5,7 +5,7 @@ namespace TicTacToeGame
     class TicTacToeMain
     {
 
-
+        // Returns menu choise of player
         static int MainMenu(string gameTitle, string[] menuComponents)
         {
             int choise = 0;
@@ -58,6 +58,286 @@ namespace TicTacToeGame
 
             return choise;
         }
+        
+        // Returns value of combinations on the field
+        static void CheckCombinations(char[][] field,ref int combinationZero,ref int combinationCross)
+        {
+            combinationZero = 0;
+            combinationCross = 0;
+
+            // Brute force of all possible combinations for one cell
+
+            for (int i = 0;i < field.Length;i++)
+            {
+                for (int l = 0;l < field.Length - 2;l++)
+                {
+                    if (field[i][l] == field[i][l + 1] && field[i][l] == field[i][l + 2])
+                    {
+                        if (field[i][l] == '0')
+                        {
+                            combinationZero++;
+                            l += 2;
+                        }
+                        else if (field[i][l] == '*')
+                        {
+                            combinationCross++;
+                            l += 2;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int l = 0; l < field.Length - 2; l++)
+                {
+                    if (field[l][i] == field[l + 1][i] && field[l][i] == field[l + 2][i])
+                    {
+                        if (field[l][i] == '0')
+                        {
+                            combinationZero++;
+                            l += 2;
+                        }
+                        else if (field[l][i] == '*')
+                        {
+                            combinationCross++;
+                            l += 2;
+                        }
+                    }
+                }
+            }
+
+            for (int i = field.Length - 3; i >=  -field.Length + 3; i--)
+            {
+                for (int l = 0; l < 3 + i; l++)
+                {
+                    if (l + 2 < i + 3 && field[i][l] == field[i+1][l+1] && field[i][l] == field[i + 2][l + 2])
+                    {
+                        if (field[l][i] == '0')
+                        {
+                            combinationZero++;
+                            l += 2;
+                        }
+                        else if  (field[l][i] == '*')
+                        {
+                            combinationCross++;
+                            l += 2;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Checks can the game be continued
+        static bool CanBeContinued(char[][] field)
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int l = 0; l < field.Length; l++)
+                {
+                    if (field[i][l] == '.')
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // Returns the field with bot's moving
+        static void BotMove(ref char[][] field)
+        {
+            int pointCount = 0;
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int l = 0; l < field.Length; l++)
+                {
+                    if (field[i][l] == '.')
+                    {
+                        pointCount++;
+                    }
+                }
+            }
+
+            Random random = new Random();
+            int move = random.Next(pointCount);
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int l = 0; l < field.Length; l++)
+                {
+                    if (field[i][l] == '.')
+                    {
+                        move--;
+                    }
+
+                    if (move == -1)
+                    {
+                        field[i][l] = '0';
+                        return;
+                    }
+                }
+            }
+        }
+
+        // Prints field
+        static void PrintField(char[][] field)
+        {
+            Console.Write('\n');
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int l = 0; l < field.Length; l++)
+                {
+                    Console.Write(field[i][l]);
+                }
+                Console.Write('\n');
+            }
+        }
+
+        // Starts game
+        static void GameStart(bool twoPlayers)
+        {
+            // Input size of field and field prepairing
+
+            int size = 0;
+            bool isFirstTime = true;
+
+            while (size < 3 || size > 30)
+            {
+                Console.Clear();
+                if (!isFirstTime)
+                {
+                    Console.WriteLine("Incorrect size: size will be greater than 2 and less than 30. Value can not contain char.");
+                }
+                else
+                {
+                    isFirstTime = false;
+                }
+
+                Console.WriteLine("Please input size of the field:");
+
+                try
+                {
+                    size = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                { }
+            }
+
+            char[][] field = new char[size][];
+
+            for(int it = 0;it < size;it++)
+            {
+                field[it] = new char[size];
+                for (int j = 0; j < size; j++)
+                {
+                    field[it][j] = '.';
+                }
+            }
+
+            // Begin to play
+
+            while (CanBeContinued(field))
+            {
+                Console.Clear();
+
+                PrintField(field);
+
+                int x, y;
+
+                try
+                {
+                    Console.WriteLine("Player 1 x coordinate:");
+                    x = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Player 1 y coordinate:");
+                    y = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input error(press any button)");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if ((x > size && y > size) || field[x - 1][y - 1] != '.')
+                {
+                    Console.WriteLine("Please input cell that is not * or 0(press any button)");
+                    Console.ReadKey();
+                    continue;
+                }
+                field[x - 1][y - 1] = '*';
+
+                if (!CanBeContinued(field))
+                {
+                    break;
+                }
+
+                if (twoPlayers)
+                {
+                    Console.Clear();
+
+                    PrintField(field);
+
+                    try
+                    {
+                        Console.WriteLine("Player 2 x coordinate:");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Player 2 y coordinate:");
+                        y = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Input error(press any button)");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    if ((x > size && y > size) || field[x - 1][y - 1] != '.')
+                    {
+                        Console.WriteLine("Please input cell that is not * or 0(press any button)");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    field[x - 1][y - 1] = '0';
+                }
+                else
+                {
+                    BotMove(ref field);
+                }
+            }
+
+            // Conclusion
+
+            Console.Clear();
+
+            int combinationZero = 0, combinationCross = 0;
+
+            CheckCombinations(field, ref combinationZero, ref combinationCross);
+
+            if (combinationCross > combinationZero)
+            {
+                Console.WriteLine("Player 1 wins!!!(press any button)");
+            }
+            else if (combinationCross == combinationZero)
+            {
+                Console.WriteLine("Draw!!!(press any button)");
+            }
+            else 
+            {
+                if (twoPlayers)
+                {
+                    Console.WriteLine("Player 2 wins!!!(press any button)");
+                }
+                else
+                {
+                    Console.WriteLine("Bot wins!!!(press any button)");
+                }
+            }
+            Console.ReadKey();
+        }
 
         static void Main(string[] args)
         {
@@ -70,8 +350,10 @@ namespace TicTacToeGame
                 switch (choise)
                 {
                     case 0:
+                        GameStart(false);
                         break;
                     case 1:
+                        GameStart(true);
                         break;
                     case 2:
                         isRunning = false;
